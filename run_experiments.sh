6cd14c975a03c8b87cec7ec9eb321bc5cc57deed
minjun <wrrtewdqsw@gmail.com>
2026-05-10 10:40:01 +0000

    .

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=1
 
+python train.py --BasicSettings.Env_name "ALE/PrivateEye-v5"
+python train.py --BasicSettings.Env_name "ALE/Qbert-v5"    # dreamerV3가 drama보다 점수가 높은 게임
+python train.py --BasicSettings.Env_name "ALE/Breakout-v5" # dreamerV3가 drama보다 점수가 높음 게임
 python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
 python train.py --BasicSettings.Env_name "ALE/Alien-v5"
 python train.py --BasicSettings.Env_name "ALE/Amidar-v5"
 python train.py --BasicSettings.Env_name "ALE/Asterix-v5"
-python train.py --BasicSettings.Env_name "ALE/PrivateEye-v5"
