43c18781adcefa71865508b20ba261f8484ec072
ìe <wrrtewdqsw@gmail.com>
2026-05-12 06:07:42 +0000

    이제 데모를 켜고 실험해볼 예정.

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=1
 
+
+
+python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
+python train.py --BasicSettings.Env_name "ALE/Alien-v5"
+python train.py --BasicSettings.Env_name "ALE/Amidar-v5"
+
+
 python train.py --BasicSettings.Env_name "ALE/PrivateEye-v5" # dreamerV3가 drama보다 점수가 높은 게임
 
 python train.py --BasicSettings.Env_name "ALE/Asterix-v5"        # dreamerV3가 drama보다 점수가 낮은 게임
@@ -12,8 +19,3 @@ python train.py --BasicSettings.Env_name "ALE/BankHeist-v5"  # dreamerV3가 dram
 python train.py --BasicSettings.Env_name "ALE/Freeway-v5"        # dreamerV3가 drama보다 점수가 낮은 게임
 python train.py --BasicSettings.Env_name "ALE/ChopperCommand-v5" # dreamerV3가 drama보다 점수가 낮은 게임
 python train.py --BasicSettings.Env_name "ALE/MsPacman-v5"       # dreamerV3가 drama보다 점수가 낮은 게임
-
-python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
-python train.py --BasicSettings.Env_name "ALE/Alien-v5"
-python train.py --BasicSettings.Env_name "ALE/Amidar-v5"
-python train.py --BasicSettings.Env_name "ALE/Asterix-v5"
