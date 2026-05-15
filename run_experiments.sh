fc287a221c718641660ebe369d0af31b6e7242e4
choemj <choemj@kaist.ac.kr>
2026-05-15 15:50:31 +0000

    .

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,13 +4,10 @@ export CUDA_VISIBLE_DEVICES=1
 
 
 python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
-python train.py --BasicSettings.Env_name "ALE/Alien-v5"
-python train.py --BasicSettings.Env_name "ALE/Amidar-v5"
-
+python train.py --BasicSettings.Env_name "ALE/Asterix-v5"        # dreamerV3가 drama보다 점수가 낮은 게임
 
 python train.py --BasicSettings.Env_name "ALE/PrivateEye-v5" # dreamerV3가 drama보다 점수가 높은 게임
 
-python train.py --BasicSettings.Env_name "ALE/Asterix-v5"        # dreamerV3가 drama보다 점수가 낮은 게임
 
 python train.py --BasicSettings.Env_name "ALE/Qbert-v5"      # dreamerV3가 drama보다 점수가 높은 게임
 python train.py --BasicSettings.Env_name "ALE/Breakout-v5"   # dreamerV3가 drama보다 점수가 높음 게임
@@ -19,3 +16,6 @@ python train.py --BasicSettings.Env_name "ALE/BankHeist-v5"  # dreamerV3가 dram
 python train.py --BasicSettings.Env_name "ALE/Freeway-v5"        # dreamerV3가 drama보다 점수가 낮은 게임
 python train.py --BasicSettings.Env_name "ALE/ChopperCommand-v5" # dreamerV3가 drama보다 점수가 낮은 게임
 python train.py --BasicSettings.Env_name "ALE/MsPacman-v5"       # dreamerV3가 drama보다 점수가 낮은 게임
+
+python train.py --BasicSettings.Env_name "ALE/Alien-v5"
+python train.py --BasicSettings.Env_name "ALE/Amidar-v5"
