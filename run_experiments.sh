d0aa3d0fa96eaa03995eb181baba9e984f0b5ddc
choemj <choemj@kaist.ac.kr>
2026-05-17 05:40:01 +0000

    .

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,6 +3,8 @@ export CUDA_VISIBLE_DEVICES=1
 
 
 
+python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
+python train.py --BasicSettings.Env_name "ALE/Asterix-v5"        # dreamerV3가 drama보다 점수가 낮은 게임
 
 python train.py --BasicSettings.Env_name "ALE/Qbert-v5"      # dreamerV3가 drama보다 점수가 높은 게임
 python train.py --BasicSettings.Env_name "ALE/Breakout-v5"   # dreamerV3가 drama보다 점수가 높음 게임
@@ -12,8 +14,6 @@ python train.py --BasicSettings.Env_name "ALE/Freeway-v5"        # dreamerV3가 
 python train.py --BasicSettings.Env_name "ALE/ChopperCommand-v5" # dreamerV3가 drama보다 점수가 낮은 게임
 python train.py --BasicSettings.Env_name "ALE/MsPacman-v5"       # dreamerV3가 drama보다 점수가 낮은 게임
 
-python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
-python train.py --BasicSettings.Env_name "ALE/Asterix-v5"        # dreamerV3가 drama보다 점수가 낮은 게임
 
 python train.py --BasicSettings.Env_name "ALE/PrivateEye-v5" # dreamerV3가 drama보다 점수가 높은 게임
 
